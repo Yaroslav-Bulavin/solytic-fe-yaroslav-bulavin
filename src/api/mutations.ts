ca63e21e80d9c191e1831a0739f2d0601a0725d8
Yaroslav-Bulavin <zpbull1@gmail.com>
2023-05-02 16:07:18 +0300

    remove unnecessary

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -1,11 +1,5 @@
 import gql from 'graphql-tag';
 
-// const UserLoginInput = gql`
-//   input UserLoginInput {
-//     emailAddress: String!
-//     password: String!
-//   }
-// `;
 export const LOGIN_USER = gql`
   mutation LoginUser($email: String!, $password: String!) {
     loginUser(email: $email, password: $password) {
